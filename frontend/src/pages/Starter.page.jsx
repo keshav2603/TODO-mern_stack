c2c814eaa72d90d9b125ce0596603383f3f085b6
keshav2603 <keshavsingla2603@gmail.com>
2024-07-24 12:34:10 +0530

    frontend started

---
+++ frontend/src/pages/Starter.page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function StarterPage(){
+    return (
+        <>
+        <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen">
+        <img src="/assets/heroImage.png" alt="" className="w-96" />  
+        </div>
+                
+        </>
+    )
+}
