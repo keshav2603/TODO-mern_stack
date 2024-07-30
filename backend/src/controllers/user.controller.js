e511ca54bb03132733993df33227511a397f06f9
keshav2603 <keshavsingla2603@gmail.com>
2024-07-30 10:58:05 +0530

    some error in get user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -159,7 +159,7 @@ const refreshAccessToken = AsyncHandler(async (req, res) => {
     }
 });
 
-const getCurrentUser = asyncHandler( async(req, res) => {
+const getCurrentUser = AsyncHandler( async(req, res) => {
     return res.status(200)
     .json(new ApiResponse(200, req.user, "current user fetch successfully!"));
 })
