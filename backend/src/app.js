48ae000206601a727889e283a5682764a7ad010b
keshav2603 <keshavsingla2603@gmail.com>
2024-08-01 16:39:39 +0530

    cores setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,9 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    credentials: true
+}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
