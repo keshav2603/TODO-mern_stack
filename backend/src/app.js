5a4ec5d2e43bf8c2a54a7aa71afd0c3f216ad618
keshav2603 <keshavsingla2603@gmail.com>
2024-08-01 10:39:06 +0530

    ok

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,20 @@
 import express from "express";
 import router from "./routes/todo.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
 
-app.use(express.json({limit:"16kb"}));
+app.use(cors({
+    origin: 'http://localhost:5173', // Replace with your actual frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.use("/api/v1/todo", router);
 
-app.use("/api/v1/todo", router)
-
-
-
-export {app};
+export { app };
